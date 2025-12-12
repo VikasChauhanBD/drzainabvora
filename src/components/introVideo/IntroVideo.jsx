7ead1f1b4f4947f4484608093d1d21dcfe1e29d4
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2025-12-12 16:01:26 +0530

    intro video section updated

--- src/components/introVideo/IntroVideo.jsx
+++ src/components/introVideo/IntroVideo.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./IntroVideo.css";
 
+// function IntroVideo({ onEnd }) {
+//   return (
+//     <div className="intro-video-container">
+//       <video className="intro-video" autoPlay muted onEnded={onEnd}>
+//         <source
+//           src="https://res.cloudinary.com/dc7y7ypmm/video/upload/v1765534307/intro-video_nc0lem.mp4"
+//           type="video/mp4"
+//         />
+//       </video>
+//     </div>
+//   );
+// }
+
 function IntroVideo({ onEnd }) {
   return (
     <div className="intro-video-container">
-      <video className="intro-video" autoPlay muted onEnded={onEnd}>
+      <video
+        className="intro-video"
+        autoPlay
+        muted
+        playsInline
+        preload="auto"
+        onEnded={onEnd}
+      >
         <source
           src="https://res.cloudinary.com/dc7y7ypmm/video/upload/v1765534307/intro-video_nc0lem.mp4"
           type="video/mp4"
