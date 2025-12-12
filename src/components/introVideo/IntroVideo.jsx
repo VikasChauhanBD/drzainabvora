703d56c35a710b0e79f9576f086b0f8b0fa32368
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2025-12-12 15:44:27 +0530

    video url added

--- src/components/introVideo/IntroVideo.jsx
+++ src/components/introVideo/IntroVideo.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./IntroVideo.css";
-import Intro from "../../assets/intro-video.mp4";
 
 function IntroVideo({ onEnd }) {
   return (
     <div className="intro-video-container">
       <video className="intro-video" autoPlay muted onEnded={onEnd}>
-        <source src={Intro} type="video/mp4" />
+        <source
+          src="https://res.cloudinary.com/dc7y7ypmm/video/upload/v1765534307/intro-video_nc0lem.mp4"
+          type="video/mp4"
+        />
       </video>
     </div>
   );
