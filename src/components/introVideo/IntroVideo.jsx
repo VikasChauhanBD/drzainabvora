9210336b86af1ebf8da2a87f38f1631ccdb5ee2d
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2025-12-15 16:04:15 +0530

    logo changed

--- src/components/introVideo/IntroVideo.jsx
+++ src/components/introVideo/IntroVideo.jsx
@@ -13,7 +13,7 @@ function IntroVideo({ onEnd }) {
         onEnded={onEnd}
       >
         <source
-          src="https://res.cloudinary.com/dc7y7ypmm/video/upload/v1765536331/intro-video_xd4qoc.mp4"
+          src="https://res.cloudinary.com/dc7y7ypmm/video/upload/v1765794423/intro-video_rh90eh.mp4"
           type="video/mp4"
         />
       </video>
