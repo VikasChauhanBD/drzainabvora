7614fc3f6fe5ce17e24c417a05871c86d9c7473b
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2025-12-26 10:47:05 +0530

    content updated of BTR on home page

--- src/components/btr/BTR.jsx
+++ src/components/btr/BTR.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./BTR.css";
+import { NavLink } from "react-router-dom";
 import BtrImage from "../../assets/images/btr-image.webp";
 
 function BTR() {
@@ -11,25 +12,17 @@ function BTR() {
         </div>
         <div className="btr-content">
           <h1 className="btr-heading">About BTR</h1>
-          <h2>
-            A Radiologist Who Teaches with Clarity, Precision, and Purpose
-          </h2>
+          <h2>A faculty who teached with Clarity, Precision and Purpose.</h2>
           <p>
-            Dr. Zainab Vora is a renowned radiologist whose passion for teaching
-            has shaped the journeys of thousands of aspiring medical
-            professionals. An alumna of AIIMS New Delhi, she scored Rank 1 at
-            AIIMS in May 2015 and Rank 1 at NIMHANS in March 2015. She completed
-            her MBBS and MD in Radiology, earned her DNB, and refined her
-            expertise as a Senior Resident in Radiodiagnosis at AIIMS.
+            Dr. Zainab Vora is a renowned radiologist and passionate educator,
+            an alumna of AIIMS New Delhi with Rank 1 at AIIMS (May 2015) and
+            NIMHANS (March 2015). She has completed her MBBS, MD (Radiology),
+            DNB, and served as a Senior Resident at AIIMS. Known for her
+            expertise in CT, MRI, ultrasound, Doppler, and interventional
+            radiology, she is a leading faculty at Vidya Jeevan, guiding
+            students for FMGE, NEET PG, and INICET with clarity and precision.
           </p>
-
-          <p>
-            Her strong command over CT, MRI, ultrasound, Doppler, and
-            interventional radiology is reflected in her clear, structured, and
-            student-friendly teaching. As a leading faculty at Vidya Jeevan, she
-            continues to inspire and guide learners preparing for FMGE, NEET PG,
-            and INICET.
-          </p>
+          <NavLink to="/btr"> See why students trust BTR</NavLink>
         </div>
       </div>
     </div>
