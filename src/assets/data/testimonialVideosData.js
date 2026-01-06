729a5cb9420cff1838dd4903657daa9dc0e01b44
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-01-06 11:35:25 +0530

    New Testimonial Videos uploaded

---
+++ src/assets/data/testimonialVideosData.js
@@ -0,0 +1,140 @@
+export const testimonialVideosData = [
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677192/reel-cover-23_qb8rfo.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677193/reel-video-23_rvrgzw.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677192/reel-cover-22_c7if7t.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677194/reel-video-22_g3esnd.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677192/reel-cover-21_zuhmg0.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677194/reel-video-21_cmxyoy.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677193/reel-cover-20_upheb4.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677195/reel-video-20_jgvaxo.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677194/reel-cover-19_c2ii0b.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677195/reel-video-19_d0vjzp.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677194/reel-cover-18_sqxmg4.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677195/reel-video-18_qimztr.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767677195/reel-cover-17_f1ctn1.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767677198/reel-video-17_tevcra.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-16_m7acey.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-16_trqgrb.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071077/reel-cover-15_pirthr.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071078/reel-video-15_ut6n6n.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071080/reel-cover-14_z6axqg.png",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071081/reel-video-14_syq6sy.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071079/reel-cover-13_ixoncb.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071080/reel-video-13_mvquh4.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071081/reel-cover-12_r0nlg5.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071080/reel-video-12_vp2pfa.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071082/reel-cover-11_hm7lb4.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071085/reel-video-11_wmu6wi.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-10_vadv4x.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071083/reel-video-10_avxzhe.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-9_nxz0n1.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071078/reel-video-9_f0gxxe.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071079/reel-cover-8_zjbk1j.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071079/reel-video-8_a6xrew.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071080/reel-cover-7_fqtsgg.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071084/reel-video-7_tzad1z.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071083/reel-cover-6_qdgoyi.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071082/reel-video-6_s0bkev.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071084/reel-cover-5_ka9rv3.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071084/reel-video-5_rq1bgu.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-4_dbqlwv.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071088/reel-video-4_diyqea.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-3_ko58vl.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071086/reel-video-3_ynm0nq.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-2_ewjsgi.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-2_dqnuox.mp4",
+  },
+  {
+    image:
+      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-1_fccp8r.jpg",
+    type: "video",
+    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-1_ceyh23.mp4",
+  },
+];
--- src/components/testimonialVideos/TestimonialVideos.jsx
+++ src/components/testimonialVideos/TestimonialVideos.jsx
@@ -6,105 +6,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/effect-coverflow";
 import "./TestimonialVideos.css";
-
-const patientsEnglishData = [
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-16_m7acey.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-16_trqgrb.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-1_fccp8r.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-1_ceyh23.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-2_ewjsgi.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-2_dqnuox.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-3_ko58vl.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071086/reel-video-3_ynm0nq.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071086/reel-cover-4_dbqlwv.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071088/reel-video-4_diyqea.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071084/reel-cover-5_ka9rv3.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071084/reel-video-5_rq1bgu.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071083/reel-cover-6_qdgoyi.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071082/reel-video-6_s0bkev.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071080/reel-cover-7_fqtsgg.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071084/reel-video-7_tzad1z.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071079/reel-cover-8_zjbk1j.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071079/reel-video-8_a6xrew.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-9_nxz0n1.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071078/reel-video-9_f0gxxe.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071076/reel-cover-10_vadv4x.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071083/reel-video-10_avxzhe.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071082/reel-cover-11_hm7lb4.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071085/reel-video-11_wmu6wi.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071081/reel-cover-12_r0nlg5.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071080/reel-video-12_vp2pfa.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071079/reel-cover-13_ixoncb.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071080/reel-video-13_mvquh4.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071080/reel-cover-14_z6axqg.png",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071081/reel-video-14_syq6sy.mp4",
-  },
-  {
-    image:
-      "https://res.cloudinary.com/dc7y7ypmm/image/upload/v1767071077/reel-cover-15_pirthr.jpg",
-    type: "video",
-    url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071078/reel-video-15_ut6n6n.mp4",
-  },
-];
+import { testimonialVideosData } from "../../assets/data/testimonialVideosData";
 
 function TestimonialVideos() {
   const swiperRef = useRef(null);
@@ -179,7 +81,7 @@ function TestimonialVideos() {
             setActiveIndex(swiper.realIndex);
           }}
         >
-          {patientsEnglishData.map((data, index) => (
+          {testimonialVideosData.map((data, index) => (
             <SwiperSlide
               className={`tv-swiper-slide ${
                 playingIndex === index ? "playing" : ""
