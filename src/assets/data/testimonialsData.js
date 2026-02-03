9c93fa2ffcc565de445939b1d00d0d4ae49624fe
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-02-03 15:17:04 +0530

    commented code removed from data

--- src/assets/data/testimonialsData.js
+++ src/assets/data/testimonialsData.js
@@ -3152,59 +3152,5 @@ export const testimonials = [
       name: "Dr. Saahi",
       image: AvatarFemale,
     },
-    // {
-    //   feedback:
-    //     "This place holds immense amount of peace and positivity. Being under the guidance of Dr. Zainab Vora ma'am is a pure blessing for any aspirant in their preparation journey. Not only do you get your concepts right but you also learn what consistency, hardwork, perseverance, and showing up everyday actually means . The staff is also very kind and supportive here. Those 55 days at BTR bootcamp were the best part of my preparation journey.",
-    //   name: "Dr. Rajeswari Riya",
-    //   image: DrRajeswariRiya,
-    // },
-    // {
-    //   feedback:
-    //     "This place is so well managed,they put their heart into it and you can easily see that,the effort,the dedication. Dr. Zainab Vora ma'am and the whole team are awesome,very inspired by them.",
-    //   name: "Dr. Shubham Bansal",
-    //   image: AvatarMale,
-    // },
-    // {
-    //   feedback:
-    //     "This place is a safe haven for all NEET PG aspirants. A very positive and calming experience. Being in the presence of Dr. Zainab Vora ma'am makes this stressful phase feel so much lighter. The staff at Vidya Jeevan is the best and most accomodating, present day in and day out to help us with any issues we might face. Thank you for this beautiful experience ❤️.",
-    //   name: "Dr. Arushi Vahie",
-    //   image: DrArushiVahie,
-    // },
-    // {
-    //   feedback:
-    //     "Attended BTR at vidya jeevan. The place and its vibes are great. Everyone was extremely hardworking. Dr. Zainab Vora ma'am is great at what she does. The place feels so welcoming. The cafeteria was good too Overall, the experience was nice.",
-    //   name: "Dr. Nishtha Koushal",
-    //   image: AvatarFemale,
-    // },
-    // {
-    //   feedback:
-    //     "State of the art facility ! Amazingly built and very smoothly managed. Kudos to the team! It was a surreal experience with a different kind of positive vibe. Thanks to apurva sir,mamaji, ravi sir, ashish sir and every single member of the team who made sure that everything else was taken care of, and we just had to teach everything Dr. Zainab Vora ma'am had planned for us. 4 day offline BTR was wholesome, fulfilling and simply amazing, and the smooth functioning of vidya jeewan definitely plays a major role here !",
-    //   name: "Dr. Jasnoor Kaur",
-    //   image: AvatarFemale,
-    // },
-    // {
-    //   feedback:
-    //     "BTR Bootcamp by Dr. Zainab Vora ma'am✨❤️ This place has such positive vibes.The best thing about this place is that they not only teach you academics but life lessons too. I'll never forget this phase of my life where I have learnt so much from my teachers. I love the environment here and enthusiasm of the students. There is very cooperative staff and everyone here helps you too. Topiary is also amazing. Much Respect 🙏✨😇.",
-    //   name: "Dr. Shubh Dahiya",
-    //   image: DrShubhDahiya,
-    // },
-    // {
-    //   feedback:
-    //     "Vidya jeevan provides a very positive environment . This place is not just a architecture buildup its actually has a vibe that motivate you to push your limits and achieve everything that you dream of. Every corner tells you something and most importantly every person here wants to see you achieve , dream, believe and think big in every aspects of life... Thank you Dr. Zainab Vora ma'am and team for supporting us and lifting us through this journey... SABR ✨SHUKAR ✨STHIR✨.",
-    //   name: "Dr. Sandhya Boora",
-    //   image: AvatarFemale,
-    // },
-    // {
-    //   feedback:
-    //     "One of the best experiences ever. The team worked really hard to make to our stay comfortable and everything was managed really well. Everyone is very motivating. Loved the teaching and support staff. Thankyou for this opportunity. Thank you Dr. Zainab Vora ma'am and team for supporting us and lifting us through this journey... SABR ✨SHUKAR ✨STHIR✨.",
-    //   name: "Dr. Anushka Gupta",
-    //   image: AvatarFemale,
-    // },
-    // {
-    //   feedback:
-    //     "This place has the best people, the best atmosphere for studying. Dr. Zainab Vora ma'am is so sweet and welcoming to all the students. The cafeteria is amazing, the computer rooms, everything is perfect. Also, really good food.",
-    //   name: "Dr. Navya",
-    //   image: DrNavya,
-    // },
   ],
 ];
