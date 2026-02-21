f8103f1ba71f4b4c6da89ca857dd63f609bee206
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-02-21 15:09:27 +0530

    fixing performance

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ import NotFound from "./components/notFound/NotFound";
 
 function App() {
   const [showIntro, setShowIntro] = useState(false);
-  const [isChecking, setIsChecking] = useState(true);
 
   useEffect(() => {
     const CHANNEL_NAME = "intro_video_channel";
@@ -30,37 +29,26 @@ function App() {
       channel = new BroadcastChannel(CHANNEL_NAME);
       let otherTabExists = false;
 
-      // Listen for messages from other tabs
       const handleMessage = (event) => {
         if (event.data.type === "tab_exists") {
-          // Another tab exists, don't show video
           otherTabExists = true;
-          setShowIntro(false);
-          setIsChecking(false);
         } else if (event.data.type === "checking") {
-          // Another tab is checking, respond that we exist
           channel.postMessage({ type: "tab_exists" });
         }
       };
 
       channel.addEventListener("message", handleMessage);
-
-      // Ask if any other tab is already open
       channel.postMessage({ type: "checking" });
 
-      // Wait briefly for responses
       const timeoutId = setTimeout(() => {
         if (!otherTabExists) {
-          // No other tabs exist, check if video should play
           const hasSeenVideo = sessionStorage.getItem("hasSeenIntro");
           if (!hasSeenVideo) {
             setShowIntro(true);
           }
         }
-        setIsChecking(false);
       }, 100);
 
-      // Cleanup
       return () => {
         clearTimeout(timeoutId);
         if (channel) {
@@ -69,12 +57,10 @@ function App() {
         }
       };
     } catch (error) {
-      // BroadcastChannel not supported, just check sessionStorage
       const hasSeenVideo = sessionStorage.getItem("hasSeenIntro");
       if (!hasSeenVideo) {
         setShowIntro(true);
       }
-      setIsChecking(false);
     }
   }, []);
 
@@ -83,39 +69,28 @@ function App() {
     setShowIntro(false);
   };
 
-  // Show nothing while checking for other tabs
-  if (isChecking) {
-    return null;
-  }
-
   return (
     <>
-      {showIntro ? (
-        <IntroVideo onEnd={handleVideoEnd} />
-      ) : (
-        <BrowserRouter>
-          <Navbar />
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUsPage />} />
-            <Route path="/btr" element={<BtrPage />} />
-            <Route path="/online-btr" element={<OnlineBtrPage />} />
-            <Route path="/offline-btr" element={<OfflineBtrPage />} />
-            <Route path="/bootcamp" element={<BootcampPage />} />
-            <Route path="/conceptual-radiology" element={<CRPage />} />
-            <Route
-              path="/conceptual-radiology-books"
-              element={<CRBooksPage />}
-            />
-            <Route path="/crisp" element={<CrispPage />} />
-            <Route path="/students" element={<StudentsPage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-      )}
+      {showIntro && <IntroVideo onEnd={handleVideoEnd} />}
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/btr" element={<BtrPage />} />
+          <Route path="/online-btr" element={<OnlineBtrPage />} />
+          <Route path="/offline-btr" element={<OfflineBtrPage />} />
+          <Route path="/bootcamp" element={<BootcampPage />} />
+          <Route path="/conceptual-radiology" element={<CRPage />} />
+          <Route path="/conceptual-radiology-books" element={<CRBooksPage />} />
+          <Route path="/crisp" element={<CrispPage />} />
+          <Route path="/students" element={<StudentsPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
