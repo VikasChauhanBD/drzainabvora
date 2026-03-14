d6c8285f7bd2aef319559035012566a55a5b404e
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-03-14 22:21:15 +0530

    new image kit videos added

--- src/assets/data/testimonialVideosData.js
+++ src/assets/data/testimonialVideosData.js
@@ -349,4 +349,340 @@ export const testimonialVideosData = [
     type: "video",
     url: "https://res.cloudinary.com/dc7y7ypmm/video/upload/v1767071089/reel-video-1_ceyh23.mp4",
   },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-48.jpg?updatedAt=1773503499936",
+  //   alt: "BTR Subscriber wishing Women's Day to Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-48.mp4?updatedAt=1773503502013",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-47.jpg?updatedAt=1773503499911",
+  //   alt: "BTR Subscriber taking online session of Dr. Zainab Vora ",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-47.mp4?updatedAt=1773503503937",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-46.jpg?updatedAt=1773503499797",
+  //   alt: "Never Quit motivation by Dr. Zainab Vora Ma'am",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-46.mp4?updatedAt=1773503504501",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-45.jpg?updatedAt=1773503499956",
+  //   alt: "Student studing by BTR notes provided by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-45.mp4?updatedAt=1773503509596",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-44.jpg?updatedAt=1773503499944",
+  //   alt: "BTR Subscriber and Students with Dr. Zainab Vora Ma'am",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-44.mp4?updatedAt=1773503510605",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-43.jpg?updatedAt=1773503500108",
+  //   alt: "Test and Discussion with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-43.mp4?updatedAt=1773503508523",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-42.jpg?updatedAt=1773503500092",
+  //   alt: "Mini Test by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-42.mp4?updatedAt=1773503507678",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-41.jpg?updatedAt=1773503499846",
+  //   alt: "BTR Subscriber and Students with Dr. Zainab Vora Ma'am",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-41.mp4?updatedAt=1773503509284",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-40.jpg?updatedAt=1773503499918",
+  //   alt: "Stay focused during long lectures with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-40.mp4?updatedAt=1773503509504",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-39.jpg?updatedAt=1773503499815",
+  //   alt: "Life Lessons with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-39.mp4?updatedAt=1773503508665",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-38.jpg?updatedAt=1773503499930",
+  //   alt: "BTR Subscriber",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-38.mp4?updatedAt=1773503506348",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-37.jpg?updatedAt=1773503499781",
+  //   alt: "Motivation speech by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-37.mp4?updatedAt=1773503502353",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-36.jpg?updatedAt=1773503499787",
+  //   alt: "Motivation speech by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-36.mp4?updatedAt=1773503506636",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-35.jpg?updatedAt=1773503500127",
+  //   alt: "Dr. Zainab Vora online class session",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-35.mp4?updatedAt=1773503503154",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-34.jpg?updatedAt=1773503500332",
+  //   alt: "BTR Subscribers with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-34.mp4?updatedAt=1773503504045",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-33.jpg?updatedAt=1773503500103",
+  //   alt: "Students with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-33.mp4?updatedAt=1773503514210",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-32.jpg?updatedAt=1773503499929",
+  //   alt: "BTR Subscribers with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-32.mp4?updatedAt=1773503511019",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-31.jpg?updatedAt=1773503500096",
+  //   alt: "Motivation speech by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-31.mp4?updatedAt=1773503506431",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-30.jpg?updatedAt=1773503499919",
+  //   alt: "Student feedback for Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-30.mp4?updatedAt=1773503511517",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-29.jpg?updatedAt=1773503499918",
+  //   alt: "Ankitha BTR Subscriber",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-29.mp4?updatedAt=1773503509225",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-28.jpg?updatedAt=1773503500312",
+  //   alt: "Dr. Zainab Vora class session",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-28.mp4?updatedAt=1773503513112",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-27.jpg?updatedAt=1773503500085",
+  //   alt: "Dr. Zainab Vora class session",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-27.mp4?updatedAt=1773503510775",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-26.jpg?updatedAt=1773503499923",
+  //   alt: "Greatest comeback by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-26.mp4?updatedAt=1773503510888",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-25.jpg?updatedAt=1773503499925",
+  //   alt: "Dr. Zainab Vora life Experience",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-25.mp4?updatedAt=1773503507163",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-24.jpg?updatedAt=1773503501781",
+  //   alt: "Dr. Zainab Vora life Experience",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-24.mp4?updatedAt=1773503507383",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-23.jpg?updatedAt=1773503500217",
+  //   alt: "Student feedback for Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-23.mp4?updatedAt=1773503512058",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-22.jpg?updatedAt=1773503500113",
+  //   alt: "Dr. Zainab Vora class session",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-22.mp4?updatedAt=1773503504519",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-21.jpg?updatedAt=1773503499903",
+  //   alt: "Unboxing BTR Books",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-21.mp4?updatedAt=1773503506583",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-20.jpg?updatedAt=1773503500339",
+  //   alt: "Student motivation with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-20.mp4?updatedAt=1773503507632",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-19.jpg?updatedAt=1773503500121",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-19.mp4?updatedAt=1773503503795",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-18.jpg?updatedAt=1773503499736",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-18.mp4?updatedAt=1773503507144",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-17.jpg?updatedAt=1773503500090",
+  //   alt: "Student feedback for Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-17.mp4?updatedAt=1773503503626",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-16.jpg?updatedAt=1773503500324",
+  //   alt: "Dr. Zainab Vora class session",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-16.mp4?updatedAt=1773503513964",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-15.jpg?updatedAt=1773503499937",
+  //   alt: "BTR 3.0 Diaries",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-15.mp4?updatedAt=1773503514979",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-14.jpg?updatedAt=1773503499973",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-14.mp4?updatedAt=1773503502393",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-13.png?updatedAt=1773503503037",
+  //   alt: "BTR Subscriber exam practice",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-13.mp4?updatedAt=1773503504841",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-12.jpg?updatedAt=1773503499953",
+  //   alt: "About BTR",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-12.mp4?updatedAt=1773503502514",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-11.jpg?updatedAt=1773503500501",
+  //   alt: "BTR Subscriber exam practice",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-11.mp4?updatedAt=1773503506721",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-10.jpg?updatedAt=1773503499840",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-10.mp4?updatedAt=1773503510773",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-9.jpg?updatedAt=1773503500109",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-9.mp4?updatedAt=1773503506378",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-8.jpg?updatedAt=1773503500098",
+  //   alt: "BTR Subscriber exam preparation",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-8.mp4?updatedAt=1773503505151",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-7.jpg?updatedAt=1773503500956",
+  //   alt: "BTR Subscriber exam practice",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-7.mp4?updatedAt=1773503507057",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-6.jpg?updatedAt=1773503500345",
+  //   alt: "About Radiology by Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-6.mp4?updatedAt=1773503512775",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-5.jpg?updatedAt=1773503500148",
+  //   alt: "BTR Subscriber Interview with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-5.mp4?updatedAt=1773503505145",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-4.jpg?updatedAt=1773503499941",
+  //   alt: "BTR Subscriber exam preparation with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-4.mp4?updatedAt=1773503503998",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-3.jpg?updatedAt=1773503500340",
+  //   alt: "BTR Subscriber exam preparation with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-3.mp4?updatedAt=1773503512566",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-2.jpg?updatedAt=1773503499760",
+  //   alt: "BTR Subscriber exam preparation with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-2.mp4?updatedAt=1773503503308",
+  // },
+  // {
+  //   image:
+  //     "https://ik.imagekit.io/qqt7duc6p/reel-cover-1.jpg?updatedAt=1773503500117",
+  //   alt: "Record class with Dr. Zainab Vora",
+  //   type: "video",
+  //   url: "https://ik.imagekit.io/qqt7duc6p/reel-video-1.mp4?updatedAt=1773503509524",
+  // },
 ];
