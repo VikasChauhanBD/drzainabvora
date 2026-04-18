c45278839b70f7df298037a55a8009dd5e6573e1
VikasChauhanBD <vikas.chauhan@believersdestination.com>
2026-04-18 08:59:39 +0530

    insta posts

--- src/components/instagramFeed/InstagramFeed.jsx
+++ src/components/instagramFeed/InstagramFeed.jsx
@@ -33,7 +33,7 @@ const InstagramFeed = () => {
       <div className="instagram-grid">
         <div className="instagram-post">
           <iframe
-            src="https://www.instagram.com/p/DXMuYmXzzNM/embed"
+            src="https://www.instagram.com/p/DXPg88OjXVX/embed"
             width="100%"
             height="580"
             frameBorder="0"
@@ -45,7 +45,7 @@ const InstagramFeed = () => {
 
         <div className="instagram-post">
           <iframe
-            src="https://www.instagram.com/p/DXMAKWpASI_/embed"
+            src="https://www.instagram.com/p/DXPTNn6jDJ7/embed"
             width="100%"
             height="580"
             frameBorder="0"
@@ -57,7 +57,7 @@ const InstagramFeed = () => {
 
         <div className="instagram-post">
           <iframe
-            src="https://www.instagram.com/p/DXKPItsEtxQ/embed"
+            src="https://www.instagram.com/p/DXPPN1rD1US/embed"
             width="100%"
             height="580"
             frameBorder="0"
